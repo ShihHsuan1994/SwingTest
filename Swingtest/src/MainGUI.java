fc16b3181db1031b5c07ff0aa0516823eeeba12c
Huang Mimo <m1011241153@gmail>
2019-04-23 13:26:54 +0800

    Poker

--- Swingtest/src/MainGUI.java
+++ Swingtest/src/MainGUI.java
@@ -5,12 +5,17 @@ import javax.swing.JButton;
 import java.awt.BorderLayout;
 import javax.swing.JLabel;
 import java.awt.event.ActionListener;
+import java.util.Scanner;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
+import javax.swing.JSpinner;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
 
 public class MainGUI {
 
 	private JFrame frame;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -42,23 +47,50 @@ public class MainGUI {
 		frame = new JFrame("SwingTest");
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 25));
-		frame.getContentPane().add(lblNewLabel, BorderLayout.CENTER);
+
+		
+		JLabel label = new JLabel("請輸入發牌人數： ");
+		label.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+		label.setBounds(80, 10, 140, 16);
+		frame.getContentPane().add(label);
+		
+		textField = new JTextField();
+		textField.setBounds(206, 6, 130, 26);
+		frame.getContentPane().add(textField);
+		textField.setColumns(16);
+
+		
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
+		textArea.setText("");
+		textArea.setBounds(88, 60, 248, 140);
+		frame.getContentPane().add(textArea);
 		
 		JButton btnNewButton = new JButton("New button");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				lblNewLabel.setText("Hello 林餅乾");
+				Poker poker = new Poker();
+				String A = textField.getText();
+				String B = textArea.getText();
+				int num = Integer.parseInt(A); 
+				for(int a=1 ; a<= num ; a++) {
+					
+					B = B + poker.pokerRandom() +"\n";
+					textArea.setText(B);
+					//poker.pokerRandom();
+				}
+			
+				
+				
 			}
 		});
-		frame.getContentPane().add(btnNewButton, BorderLayout.SOUTH);
-		
-		
+		btnNewButton.setBounds(149, 232, 117, 29);
+		frame.getContentPane().add(btnNewButton);
 		
 		
 		
 	}
-
 }
